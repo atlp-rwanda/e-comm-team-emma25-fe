0f55b0fe9854a3b8478db5b295734ed38a7fb2de
Rukundojoseph <104127964+Rukundojoseph@users.noreply.github.com>
2023-03-23 05:31:10 -0800

    feat: Setup an empty react project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./assets/styles/style.css"
+import Hello from './components/component1';
+function App() {
+  return (
+    <div>
+      <Hello/>
+      <h1>front end</h1>
+    </div>
+  );
+}
+
+export default App;
