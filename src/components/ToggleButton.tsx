cbd14508811110a432fe7957012b88470f111970
Jean Damascene Uwingeneye <jeandamas85@gmail.com>
2023-03-28 12:06:43 +0200

    ch-eslint

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 const ToggleButton = () => {
-    const [on, setOn] = useState(false);
-    return <button onClick={() => setOn(!on)}>{on ? "ON" : "OFF"}</button>
+  const [on, setOn] = useState(false);
+  return <button onClick={() => setOn(!on)}>{on ? "ON" : "OFF"}</button>;
 };
 export default ToggleButton;
