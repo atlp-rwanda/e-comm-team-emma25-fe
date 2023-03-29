b26c85e432805f219e21ed26c560876f5b65dc29
Rukundojoseph <104127964+Rukundojoseph@users.noreply.github.com>
2023-03-29 05:09:31 -0800

    feat: add more add-ons

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,10 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+     '@storybook/addon-backgrounds',
+     '@storybook/addon-controls',
+     "@storybook/addon-actions",
   ],
   "framework": "@storybook/react",
   "core": {
