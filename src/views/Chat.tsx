d9298397976fa33c958eefe41d8b8e4906bef4fa
Rukundojoseph <104127964+Rukundojoseph@users.noreply.github.com>
2023-04-17 17:38:34 -0800

    feat: add navbar

---
+++ src/views/Chat.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+function Chat() {
+  return (
+    <div>
+      <Navbar iconNumber={2} />
+    </div>
+  );
+}
+
+export default Chat;
---
+++ src/views/Checkout/CheckoutSuccess.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+function CheckoutSuccess() {
+  return (
+    <div>
+      <Navbar iconNumber={1} />
+      <h2>Checkout succefully</h2>
+    </div>
+  );
+}
+
+export default CheckoutSuccess;
