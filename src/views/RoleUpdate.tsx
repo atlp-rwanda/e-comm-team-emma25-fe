8234a2454ba41e20ead04bf1a0165be852d80829
Jean Damascene Uwingeneye <jeandamas85@gmail.com>
2023-04-25 10:52:09 +0200

    RBAC page

---
+++ src/views/RoleUpdate.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+function RoleUpdate() {
+  return (
+    <div>
+      <Navbar iconNumber={2} />
+      <>
+        <h1>Welcome to the role update</h1>
+      </>
+    </div>
+  );
+}
+
+export default RoleUpdate;
