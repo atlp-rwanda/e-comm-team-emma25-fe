b5ce1a8ef6c932e04f2680bdc1f4828c468565c3
Rukundojoseph <josephrukundo2002@gmail.com>
2023-04-28 13:54:28 +0200

    feat: add make request

--- src/views/WishList.tsx
+++ src/views/WishList.tsx
@@ -4,8 +4,53 @@ import "../assets/styles/bottomNav.css";
 import { Typography } from "@mui/material";
 import Wishlistcard from "../components/wishlist/wishlistcard";
 import Box from "@mui/material/Box";
+import { AxiosClient } from "../utils/AxiosClient";
+// interface wishitems {
+//   id: number,
+//   wishlistId: number,
+//   ProductID: string,
+//   createdAt: Date,
+//   updatedAt: Date,
+//   Product: {
+//       ProductID: string,
+//       ProductName: string,
+//       ProductPrice: number,
+//       quantity: number,
+//       available: boolean,
+//       ProductDesc: string,
+//       ProductOwner: number,
+//       createdAt: Date,
+//       updatedAt: Date
+//       }
+// }
+import Cookies from "js-cookie";
+
 
 function WishList() {
+  function getCookie(name: string): string | undefined {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    const gettoken: string | undefined = Cookies.get(name);
+    return gettoken;
+  }  
+  const token: string| undefined = getCookie('token')
+  // const [Wishlistitems,setItems] = useState<wishitems[]>([])
+  console.log('getting the data:')  
+  if(token){
+    AxiosClient.get("/products/wishlist/view",{
+      headers:{
+      Authorization: `Bearer ${token}`
+      }
+    })
+    .then((response) => {
+      if (response.data.status == 200) {
+      console.log(response)
+      console.log(response.data.data.WishlistItems)
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
   return (
     <div>
       <Navbar iconNumber={5} />
