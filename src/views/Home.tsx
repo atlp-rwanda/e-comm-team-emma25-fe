61b62f266211c6e88728852763566389d1257d1e
Jean Damascene Uwingeneye <jeandamas85@gmail.com>
2023-05-05 08:48:07 +0200

    FIX: Notifications On Home Products Buttons

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,10 +3,12 @@ import Navbar from "../components/Navbar";
 import LandingPage from "../components/LandingPage";
 import AllProducts from "../components/AllProducts";
 import Footer from "../components/Footer";
+import { Toaster } from "react-hot-toast";
 
 function Home() {
   return (
     <div>
+      <Toaster />
       <Navbar iconNumber={1} />
       <LandingPage />
       <AllProducts />
